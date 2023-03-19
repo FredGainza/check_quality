4b551148787828ddc5b881b183f33e97a7ae08c2
FredGainza <contact@kopatik.fr>
2023-03-19 09:36:23 +0000

    change indentation 2

--- file_test.py
+++ file_test.py
@@ -1,22 +1,17 @@
 import pytest
 
 def test_calc_addition():
-  # Foncton test du résultat de 2+4
   output = 2+4
   assert output == 6
 
 def test_calc_substraction():
-  # Fonction test du résultat 2-4
   output = 2-4
   assert output == -2
 
 def test_calc_multiply():
-  # Fonction test du résultat 2*4
   output = 2*4
   assert output == 8
 
 def test_coucou():
-  # Fonction test qui renvoie coucou
   output = "hello"
-
-	assert output == "hello"
+  assert output == "hello"
