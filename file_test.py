542ce4eb061d5e88db6680e12a06b938eaf56724
FredGainza <contact@kopatik.fr>
2023-03-19 08:16:14 +0000

    ajout du fichier file_test.py

---
+++ file_test.py
@@ -0,0 +1,23 @@
+import pytest
+
+def test_calc_addition():
+	# Foncton test du résultat de 2+4
+	output = 2+4
+	assert output == 6
+
+def test_calc_substraction():
+	# Fonction test du résultat 2-4
+	output = 2-4
+	assert output == -2
+
+def test_calc_multiply():
+	# Fonction test du résultat 2*4
+	output = 2*4
+	asset output == 8
+
+def test_coucou()
+	# Fonction test qui renvoie coucou
+	output = "hello"
+	assert output == "hello"
+
+
